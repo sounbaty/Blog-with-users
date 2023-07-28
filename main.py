77ddecc4f796509bea89946fcd904b77afc4729b
Hossam <hossamelsounbaty@gmail.com>
2023-07-29 02:39:16 +0400

    Update requirements

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from flask_ckeditor import CKEditor
 from datetime import date
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentsForm
 from flask_gravatar import Gravatar
 from functools import wraps
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
